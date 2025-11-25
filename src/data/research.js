bfe6592937959fcb213d9cb04a8ee856d0f69042
Overmeer, Thijs <t.overmeer@tue.nl>
2025-11-25 08:02:36 +0100

    feat: add 'Revisiting Expected Possession Value in Football' research paper details and its associated image.

--- src/data/research.js
+++ src/data/research.js
@@ -1,9 +1,9 @@
 const research_data = [
     {
         id: 1,
-        title: "To be announded",
-        description: ``,
-        imageUrl: "/images/eaisi.webp"
+        title: "Revisiting Expected Possession Value in Football",
+        description: `Awarded the Best Paper Award at icSPORTS 2025. This paper introduces a new U-Net architecture for Expected Possession Value (EPV) in football, incorporating ball height and decomposing pass values into reward and risk. It also establishes the OJN-Pass-EPV benchmark to quantitatively assess model quality.`,
+        imageUrl: "/images/icsports_2025.png"
     },
       // More projects...
     ];
